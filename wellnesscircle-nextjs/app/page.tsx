7e1aaf6b88f7f5fc883c742dcedd092010c95186
Liquelaliqour <lewismomanyi34@gmail.com>
2024-09-04 19:25:54 +0300

    Quick fix plus more stlying

--- wellnesscircle-nextjs/app/page.tsx
+++ wellnesscircle-nextjs/app/page.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import MaxWidthWrapper from "./components/MaxWidthWrapper";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
@@ -8,6 +10,17 @@ export default function Home() {
         wellness circle is now public !
       </p>
     </div>
+    <h1 className='max-w-4xl text-5xl font-bold md:text-6xl 1g:text-7xl'>
+      Chat with your <span className='text-blue-600'>friends</span> in seconds.
+    </h1>
+    <p className='mt-5 max-w-prose text-zinc-700 sm:tect-lg'>
+      Wellness Circle is a platform that allows you to chat others on health related topics and share insightfull knowledge on how to address certain 
+      health issues naturally.
+    </p>
+
+    <Link href='/dashboard' target='_blank'>
+    Get started
+    </Link>
   </MaxWidthWrapper>
   );
 }
--- wellnesscircle-nextjs/tailwind.config.ts
+++ wellnesscircle-nextjs/tailwind.config.ts
@@ -20,11 +20,12 @@ const config: Config = {
         '8x1': '1400px',
       },
       colors: {
-        border: 'hsl(var)(--border))',
-        input: 'hsl(var)(--input))',
-        ring: 'hsl(var)(--ring))',
-        background: 'hsl(var)(--background))',
-        foreground: 'hsl(var)(--foreground))',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))'
@@ -56,18 +57,18 @@ const config: Config = {
       },
       borderRadius: {
         lg: 'var(--radius)',
-        md: 'calc(var(--radius) -2px)',
-        sm: 'calc(var(--radius) -4px)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: 
       {
         'accordion-down': {
-          from: { height: 0 },
+          from: { height: '0' },
           to: { height: 'var(--radix-accordion-content-height)'},
         },
         'accordion-up': {
           from: { height:  'var(--radix-accordion-content-height)'},
-          to: { height: 0 },
+          to: { height: '0' },
         },
       },
       animation: {
